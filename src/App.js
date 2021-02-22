6b14e9192d3a594964742f98299812fa85f92d34
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-22 10:58:40 -0700

    Created general provider setup and redux attachment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Meme from "./Meme";
+import NewMemeForm from "./NewMemeForm";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const memes = useSelector(state => state.memes);
+  const dispatch = useDispatch();
+
+  function addMeme(newMeme) {
+    dispatch({ type: "ADD_MEME", meme: newMeme });
+  }
+
+  function deleteMeme(id) {
+    dispatch({ type: "DELETE_MEME", id });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Meme Generator!</h1>
+        <NewMemeForm addMeme={addMeme}/>
+        <hr />
+        <div className="App-MemeList container">
+          <div className="row">
+            { memes.map(meme => (
+              <Meme 
+                  key={meme.id}
+                  firstLine={meme.firstLine}
+                  secondLine={meme.secondLine}
+                  image={meme.image}
+                  deleteMeme={() => deleteMeme(meme.id)}
+              />
+            ))}
+          </div>
+        </div>
       </header>
     </div>
   );
